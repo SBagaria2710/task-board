74360db5ddcf00293f95346d5599ff3c36c6d643
SBagaria2710 <shashwawtforwork@gmail.com>
2021-07-23 12:03:38 +0530

    Copy changes in about section

--- Modals/AboutModal.js
+++ Modals/AboutModal.js
@@ -14,15 +14,18 @@ function AboutModal({ onClose }) {
   <Modal show onClose={onClose} modalClass={s.modalClass} modalContentClass={s.modalContentClass}>
     <div className={s.wrapper}>
       <div className={s.container}>
-        <div className={s.version}>v1.0.0</div>
-        <div className={s.about}>About</div>
-        <div>Task board for managing task status.</div>
+        <h1>Hey 👋</h1>
+        <div className={s.about}>v1.0.0</div>
+        <p>Manage your daily todos with this kanban style task board!</p>
       </div>
-      <div className={s.socialLinkContainer}>
-        <a href={socialLink.linkedIn} target="_blank">LinkedIn</a>
-        <a href={socialLink.twitter} target="_blank">Twitter</a>
-        <a href={socialLink.github} target="_blank">Github</a>
-        <a href={socialLink.codepen} target="_blank">CodePen</a>
+      <div>
+        <p style={{textAlign: 'center'}}>Feel free to connect and drop a suggestion 👇</p>
+        <div className={s.socialLinkContainer}>
+          <a href={socialLink.linkedIn} target="_blank">LinkedIn</a>
+          <a href={socialLink.twitter} target="_blank">Twitter</a>
+          <a href={socialLink.github} target="_blank">Github</a>
+          <a href={socialLink.codepen} target="_blank">CodePen</a>
+        </div>
       </div>
     </div>
   </Modal>);
